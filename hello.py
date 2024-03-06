37aeecc9dc03f20751d9886355704faacff594c0
= <=>
2024-03-06 15:50:26 -0600

    factorial of a number

--- hello.py
+++ hello.py
@@ -1,2 +1,12 @@
+import sys
 print("Welcome to my first git python script")
-print("Good Bye")
+print("Factorial of a number")
+def fact(n):
+    if n == 1:
+         return 1
+    else:
+         return n*fact(n-1)
+
+n_fact = fact(int(sys.argv[1]))
+print("Factorial = ", n_fact)
+
